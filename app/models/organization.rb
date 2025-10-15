275d0cf16e87a97253795bfb47713cad42a425c0
Orlando Sanchez <or.sanchez100@gmail.com>
2025-10-15 18:43:17 -0400

    Add validations to organization model and update its atributes to not nullable

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,4 @@
 class Organization < ApplicationRecord
+	validates :name, presence: true
+    validates :description, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_15_213457) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_15_220500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -29,8 +29,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_15_213457) do
   end
 
   create_table "organizations", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
+    t.string "name", null: false
+    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
