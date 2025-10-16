7a20f2ab4f584776db120307b031347bc8671b89
Orlando Sanchez <or.sanchez100@gmail.com>
2025-10-16 15:51:54 -0400

    Make name in UserType not null and unique

--- app/models/user_type.rb
+++ app/models/user_type.rb
@@ -1,2 +1,3 @@
 class UserType < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_15_220500) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_16_175013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -36,9 +36,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_15_220500) do
   end
 
   create_table "user_types", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_user_types_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
