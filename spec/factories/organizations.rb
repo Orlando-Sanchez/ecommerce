5b8fef92cc5998d109df407728060f20bb88e1db
Orlando Sanchez <or.sanchez100@gmail.com>
2025-10-16 13:43:13 -0400

    Add organization model tests and remove bullet gem configuration from test

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :organization do
-    
+    name { Faker::Company.unique.name }
+    description { Faker::Company.catch_phrase }
   end
 end
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:organization) { build(:organization) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:description) }
+  end
 end
